4db08ece968a64a1246e1ad89e4d89cfd95f12ad
lauribohm <lauri.bohm@gmail.com>
2021-05-30 20:24:11 +0300

    update

--- logistic-regression.py
+++ logistic-regression.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import sys
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import RFE
